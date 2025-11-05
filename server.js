57a9b130f99f5a09fe4436e72008c523979716df
Eva-Contreras <evacontreras425@gmail.com>
2025-11-05 16:48:59 -0600

    Integrate inventory routes into server
    Added inventory routes to the server.

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT // 3306 por defecto
 });
 
+// Lineas de inventario
+const inventarioRoutes = require('./inventarioroutes')(pool);
+app.use('/api/inventario', inventarioRoutes);
+// Fin lineas inventario
+
 // Función para verificar la conexión al iniciar el servidor
 async function testDbConnection() {
   try {
